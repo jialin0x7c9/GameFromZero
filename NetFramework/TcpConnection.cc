dbc432a6078224124945463563bfe551f2eef93a
xujialin <jialinxu1993@gmail.com>
2020-03-19 23:30:40 +0800

    continue TcpConnection

--- NetFramework/TcpConnection.cc
+++ NetFramework/TcpConnection.cc
@@ -38,3 +38,185 @@ void TcpConnection::send(Buffer *buf)
 
     }
 }
+
+void TcpConnection::sendInLoop(const void *data, size_t len)
+{
+    ssize_t nwrote = 0;
+    size_t remaining = len;
+    bool faultError = false;
+    if (state_ == kDisconnected)
+    {
+        return;
+    }
+    if (!channel_->isWriting() && outputBuffer_.readableBytes() == 0)
+    {
+        nwrote = write(channel_->fd(), data, len);
+        if (nwrote >= 0)
+        {
+            remaining = len - nwrote;
+            if (remaining == 0 && writeCompleteCallback_)
+            {
+                writeCompleteCallback_();
+            }
+        }
+        else
+        {
+            nwrote = 0;
+            if (errno != EWOULDBLOCK)
+            {
+                if (errno == EPIPE || errno == ECONNRESET)
+                {
+                    faultError = true;
+                }
+            }
+        }
+    }
+
+    if (!faultError && remaining > 0)
+    {
+        size_t oldLen = outputBuffer_.readableBytes();
+        if (oldLen + remaining >= highWaterMark_ 
+            && oldLen < highWaterMark_
+            && highWaterMarkCallback_)
+        {
+
+        }
+        outputBuffer_.append(static_cast<const char*>(data)+nwrote, remaining);
+        if (!channel_->isWriting())
+        {
+            channel_->enableWriting();
+        }
+    }
+}
+
+void TcpConnection::shutdown()
+{
+    if (state_ == kConnected)
+    {
+        setState(kDisconnecting);
+    }
+}
+
+void TcpConnection::shutdownInLoop()
+{
+    if (!channel_->isWriting())
+    {
+        socket_->shutdownWrite();
+    }
+}
+
+void TcpConnection::setTcpNoDelay(bool on)
+{
+    socket_->setTcpnoDelay(on);
+}
+
+
+void TcpConnection::startRead()
+{
+    loop_->runInLoop(std::bind(&TcpConnection::startReadInLoop, this));
+}
+
+void TcpConnection::startReadInLoop()
+{
+    if (!reading_ || !channel_->isReading())
+    {
+        channel_->enableReading();
+        reading_ = true;
+    }
+}
+
+void TcpConnection::stopRead()
+{
+    loop_->runInLoop(std::bind(&TcpConnection::stopReadInLoop, this));
+}
+
+void TcpConnection::stopReadInLoop()
+{
+    if (reading_ || channel_->isReading())
+    {
+        channel_->disableReading();
+        reading_ = false;
+    }
+}
+
+
+void TcpConnection::connectionEstablished()
+{
+    setState(kConnected);
+    channel_->tie(shared_from_this());
+    channel_->enableReading();
+    connectionCallback_(shared_from_this());
+}
+
+void TcpConnection::connectionDestroyed()
+{
+    if (state_ == kConnected)
+    {
+        setState(kDisconnected);
+        channel_->disableAll();
+        connectionCallback_(shared_from_this());
+    }
+    channel_->remove();
+}
+
+void TcpConnection::handleRead(int receiveTime)
+{
+    int savedErrno = 0;
+    ssize_t n = inputBuffer_.readFd(channel_->fd(), &savedErrno);
+    if (n > 0)
+    {
+        messageCallback_(shared_from_this(), &inputBuffer_, receiveTime);
+    }
+    else if (n == 0)
+    {
+        handleClose();
+    }
+    else
+    {
+        errno = savedErrno;
+        handleError();
+    }
+}
+
+void TcpConnection::handleWrite()
+{
+    if (channel_->isWriting())
+    {
+        ssize_t n = write(channel_->fd(), outputBuffer_.peek(), outputBuffer_.readableBytes());
+        if (n > 0)
+        {
+            outputBuffer_.retrieve(n);
+            if (outputBuffer_.readableBytes() == 0)
+            {
+                channel_->disableWriting();
+                if (writeCompleteCallback_)
+                {
+                    loop_->queueInLoop(std::bind(writeCompleteCallback_, shared_from_this()));
+                }
+                if (state_ == kDisconnecting)
+                {
+                    shutdownInLoop();
+                }
+            }
+        }
+        else
+        {
+
+        }
+    }
+    else
+    {
+
+    }
+}
+
+void TcpConnection::handleClose()
+{
+    setState(kDisconnected);
+    channel_->disableAll();
+    TcpConnectionPtr guardThis(shared_from_this());
+    connectionCallback_(guardThis);
+    closeCallback_(guardThis);
+}
+
+
